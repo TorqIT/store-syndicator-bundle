3a874af25d5c8479a1debba7309b4d95c009d61d
Cameron Jenkins <cameron.jenkins@torqit.ca>
2023-06-06 15:13:38 -0300

    add exclude option to frontend

--- src/Resources/public/js/pimcore/helpers/workspacePicker.js
+++ src/Resources/public/js/pimcore/helpers/workspacePicker.js
@@ -1,9 +1,13 @@
-pimcore.registerNS("pimcore.plugin.storeExporterDataObject.helpers.workspace.object");
-pimcore.plugin.storeExporterDataObject.helpers.workspace.object = Class.create(pimcore.plugin.datahub.workspace.abstract, {
-
+pimcore.registerNS(
+  "pimcore.plugin.storeExporterDataObject.helpers.workspace.object"
+);
+pimcore.plugin.storeExporterDataObject.helpers.workspace.object = Class.create(
+  pimcore.plugin.datahub.workspace.abstract,
+  {
     type: "object",
-    initialize: function (parent) {
-        this.parent = parent;
-        this.workspaces = this.parent.data.products.products;
+    initialize: function (parent, data) {
+      this.parent = parent;
+      this.workspaces = data;
     },
-});
+  }
+);
